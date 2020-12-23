31d1e463824ac260cd351ef595d12911e66a63d7
Emil <emil@logismico.com>
2020-12-23 19:43:18 +0530

    Add the modal on the existing page

--- src/BasicCalander.jsx
+++ src/BasicCalander.jsx
@@ -5,7 +5,7 @@ import moment from 'moment';
 import events from './Events/Events';
 import * as dates from './utils/Dates';
 import _, { set } from 'lodash';
-import Modal from "react-bootstrap/Modal";
+import Modal from "react-modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -18,70 +18,94 @@ const ColoredDateCellWrapper = ({ children }) =>
       backgroundColor: 'lightblue',
     },
   })
+  const customStyles = {
+    content : {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight : '-50%',
+      transform: 'translate(-50%, -50%)'
+    }
+  };
 
 function BasicCalander(props) {
+  let subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  const [title, setTitle] = React.useState('');
   const [state, setState] = useState({
-      events: _.cloneDeep(events),
+      events: _.cloneDeep([]),
       dayLayoutAlgorithm: 'no-overlap'
   })
-  const [isOpen, setIsOpen] = React.useState(false);
-  const [title, setTitle] = React.useState("Transitioning...");
-  // const customStyles = {
-  //   content : {
-  //     top                   : '50%',
-  //     left                  : '50%',
-  //     right                 : 'auto',
-  //     bottom                : 'auto',
-  //     marginRight           : '-50%',
-  //     transform             : 'translate(-50%, -50%)'
-  //   }
-  // };
-  // let subtitle;
-  // const [modalIsOpen,setIsOpen] = React.useState(false);
 
-  const showModal = () => {
+  const openModal = () => {
     setIsOpen(true);
-  };
-
-  const hideModal = () => {
+  }
+ 
+  const afterOpenModal = () => {
+    subtitle.style.color = '#f00';
+  }
+ 
+  const closeModal = () => {
     setIsOpen(false);
-    setTitle("Transitioning...");
-  };
-
-  const modalLoaded = () => {
-    setTitle("Modal Ready");
-  };
+  }
 
   const handleSelect = ({ start, end }) => {
     setIsOpen(true);
-    const title = window.prompt('New Event name')
-    if (title)
-      setState({
+    setState({
         events: [
           ...state.events,
           {
             start,
-            end,
-            title,
+            end
           },
         ],
       })
   }
 
+  const handleSubmit = () => {
+    setState({
+      events: [
+        ...state.events,
+        {
+          title
+        }
+          
+      ],
+    })
+    setIsOpen(false);
+  }
+
+
   return (
     <>
-      <Calendar
+    <Calendar
         selectable
         localizer={localizer}
         events={state.events}
         defaultView={Views.WEEK}
         scrollToTime={new Date(1970, 1, 1, 6)}
         defaultDate={new Date(2015, 3, 12)}
-        onSelectEvent={event => alert(event.title)}
+        onSelectEvent={event => alert(title)}
         onSelectSlot={handleSelect}
         dayLayoutAlgorithm={state.dayLayoutAlgorithm}
       />
-
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h4 ref={_subtitle => (subtitle = _subtitle)}>Event Name</h4>
+          <form>
+            <input onChange={(e) => setTitle(e.target.value)} />
+          </form>
+          <br />
+          <button type="button" onClick={handleSubmit}>submit</button> <pr>     </pr>
+          <button type="button" onClick={closeModal}>close</button>
+      </Modal>
+    
     </>
   )
 } 
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 
-const Modal = ({ handleClose, show, children }) => {
-    const showHideClassName = show ? "modal display-block" : "modal display-none";
+const Modal = (Props) => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
   
     return (
-      <div className={showHideClassName}>
-        <section className="modal-main">
-          {children}
-          <button onClick={handleClose}>close</button>
-        </section>
-      </div>
+        <>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+  
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     );
   };
 
